0975c20ce1a8b721396ba7fba9887ffca44c57ac
Taras <taras19041@gmail.com>
2020-05-10 07:23:59 +0300

    add JPA properties and refactor package name

--- src/main/java/com/kovaliv/lab2/Main.java
+++ src/main/java/com/kovaliv/lab2/Main.java
@@ -1,8 +1,10 @@
-package lab2;
+package com.kovaliv.lab2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
--- src/main/java/com/kovaliv/lab2/controllers/MainController.java
+++ src/main/java/com/kovaliv/lab2/controllers/MainController.java
@@ -1,4 +1,4 @@
-package lab2.controllers;
+package com.kovaliv.lab2.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
