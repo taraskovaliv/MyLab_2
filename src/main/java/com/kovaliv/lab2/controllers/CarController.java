eef48a61306a9e9333e3580230cb47b2120863ca
Taras <taras19041@gmail.com>
2020-05-13 06:47:31 +0300

    add CarController

---
+++ src/main/java/com/kovaliv/lab2/controllers/CarController.java
@@ -0,0 +1,38 @@
+package com.kovaliv.lab2.controllers;
+
+import com.kovaliv.lab2.dtos.car.AddCarDto;
+import com.kovaliv.lab2.dtos.car.CarDto;
+import com.kovaliv.lab2.services.CarService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class CarController {
+    private final CarService carService;
+
+    @GetMapping
+    public ResponseEntity<List<CarDto>> getAll() {
+        return ResponseEntity.ok(carService.getAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<CarDto> save(@RequestBody AddCarDto addCarDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(carService.save(addCarDto));
+    }
+
+    @PutMapping
+    public ResponseEntity<CarDto> update(@RequestBody CarDto carDto) {
+        return ResponseEntity.ok(carService.update(carDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> delete(@PathVariable Long id) {
+        carService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+}
