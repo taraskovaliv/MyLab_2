2f715f552c6f80b987732336ab9e8b1fd997a35a
Taras <taras19041@gmail.com>
2020-05-13 06:53:47 +0300

    add CustomerController and RentController

---
+++ src/main/java/com/kovaliv/lab2/controllers/CustomerController.java
@@ -0,0 +1,38 @@
+package com.kovaliv.lab2.controllers;
+
+import com.kovaliv.lab2.dtos.customer.AddCustomerDto;
+import com.kovaliv.lab2.dtos.customer.CustomerDto;
+import com.kovaliv.lab2.services.CustomerService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @GetMapping
+    public ResponseEntity<List<CustomerDto>> getAll() {
+        return ResponseEntity.ok(customerService.getAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<CustomerDto> save(@RequestBody AddCustomerDto addCustomerDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(customerService.save(addCustomerDto));
+    }
+
+    @PutMapping
+    public ResponseEntity<CustomerDto> update(@RequestBody CustomerDto customerDto) {
+        return ResponseEntity.ok(customerService.update(customerDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> delete(@PathVariable Long id) {
+        customerService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/com/kovaliv/lab2/controllers/RentController.java
@@ -0,0 +1,38 @@
+package com.kovaliv.lab2.controllers;
+
+import com.kovaliv.lab2.dtos.rent.AddRentDto;
+import com.kovaliv.lab2.dtos.rent.RentDto;
+import com.kovaliv.lab2.services.RentService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class RentController {
+    private final RentService rentService;
+
+    @GetMapping
+    public ResponseEntity<List<RentDto>> getAll() {
+        return ResponseEntity.ok(rentService.getAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<RentDto> save(@RequestBody AddRentDto addRentDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(rentService.save(addRentDto));
+    }
+
+    @PutMapping
+    public ResponseEntity<RentDto> update(@RequestBody RentDto rentDto) {
+        return ResponseEntity.ok(rentService.update(rentDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> delete(@PathVariable Long id) {
+        rentService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+}
